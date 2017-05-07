0395d8fcf3b2676a63014acedee787c9267079c0
poming\pomingpo <roy989898@gmail.com>
2017-05-07 16:43:41 +0800

    can use AccessibilityService to listen itself

---
+++ app/src/main/java/com/example/pomingpo/pomautoanswer/RobService.java
@@ -0,0 +1,34 @@
+package com.example.pomingpo.pomautoanswer;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by pomingpo on 2017/5/7.
+ */
+
+public class RobService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+        int eventType = event.getEventType();
+        Log.d("onAccessibilityEvent", eventType + "");
+    }
+
+    @Override
+    public void onInterrupt() {
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        AccessibilityServiceInfo serviceInfo = new AccessibilityServiceInfo();
+        serviceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        serviceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+        serviceInfo.packageNames = new String[]{"com.example.pomingpo.pomautoanswer"};
+        serviceInfo.notificationTimeout = 100;
+        setServiceInfo(serviceInfo);
+    }
+}
