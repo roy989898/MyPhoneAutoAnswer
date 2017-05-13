8a57ccfe0a26ff71236de5de927140ab63e1d712
poming\pomingpo <roy989898@gmail.com>
2017-05-13 21:18:53 +0800

    when have call come,can auto click the sms button on MX5

--- app/src/main/java/com/example/pomingpo/pomautoanswer/RobService.java
+++ app/src/main/java/com/example/pomingpo/pomautoanswer/RobService.java
@@ -2,8 +2,13 @@ package com.example.pomingpo.pomautoanswer;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
 
 /**
  * Created by pomingpo on 2017/5/7.
@@ -16,6 +21,16 @@ public class RobService extends AccessibilityService {
 
         int eventType = event.getEventType();
         Log.d("onAccessibilityEvent", eventType + "");
+        receiveCall1();
+
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                receiveCall1();
+                break;
+            default:
+                break;
+        }
+
     }
 
     @Override
@@ -25,10 +40,26 @@ public class RobService extends AccessibilityService {
     @Override
     protected void onServiceConnected() {
         AccessibilityServiceInfo serviceInfo = new AccessibilityServiceInfo();
-        serviceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        /*serviceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
         serviceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
-        serviceInfo.packageNames = new String[]{"com.example.pomingpo.pomautoanswer","com.facebook.katana","com.android.incallui","com.android.providers.telephony","com.android.server.telecom"};
+        serviceInfo.packageNames = new String[]{"com.android.incallui"};
+        serviceInfo.getCanRetrieveWindowContent()
         serviceInfo.notificationTimeout = 100;
-        setServiceInfo(serviceInfo);
+        setServiceInfo(serviceInfo);*/
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void receiveCall1() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo != null) {
+            //为了演示,直接查看了红包控件的id
+            List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByViewId("com.android.incallui:id/sms_incoming_layout");
+            nodeInfo.recycle();
+            for (AccessibilityNodeInfo item : list) {
+                item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+
+
     }
 }
